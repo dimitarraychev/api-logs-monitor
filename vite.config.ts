c9df1bdc1eecf08319fe52a19051af69dfd9c8a5
draytchev <draytchev@gmail.com>
2026-06-12 13:38:21 +0300

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,23 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  server: {
+    proxy: {
+      "/game-api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/game-api/, ""),
+      },
+      "/report-api": {
+        target: "http://localhost:3002",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/report-api/, ""),
+      },
+      "/powerbi-cron": {
+        target: "http://localhost:3003",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/powerbi-cron/, ""),
+      },
+    },
+  },
 });
